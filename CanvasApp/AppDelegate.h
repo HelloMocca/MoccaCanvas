32b6789df0164bb7a22b50af82bd76cb0dac43d3
hellomocca <hello.mocca.k@gmail.com>
2015-09-21 15:32:18 +0900

    Initial Commit

---
+++ CanvasApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CanvasApp
+//
+//  Created by mocca on 2015. 9. 21..
+//  Copyright (c) 2015년 mocca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CanvasApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CanvasApp
+//
+//  Created by mocca on 2015. 9. 21..
+//  Copyright (c) 2015년 mocca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
